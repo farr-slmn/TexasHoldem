00ce346126ff2558e5860f2987bc656ecdede84f
Jessie <farr.slmn@gmail.com>
2016-07-17 22:24:16 +0300

    Game constructor changes

--- src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Game.java
+++ src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Game.java
@@ -11,6 +11,9 @@ public class Game {
 
     public Game(int bank, Player ... players){
         this.players = new LinkedList<Player>();
+        if(players.length != 4){
+            throw new IllegalArgumentException("Player amount not equal to 4");
+        }
         for(Player p : players){
             this.players.add(p);
         }
