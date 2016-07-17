e6ec100a8384a662745631003f939781565db233
Jessie <farr.slmn@gmail.com>
2016-07-17 19:34:51 +0300

    At last...[2]

--- src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Main.java
+++ src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Main.java
@@ -1,10 +1,8 @@
 package com.innopolis.university.bootcamp2016.programmingA.texasholdem;
 
-/**
- * Created by Jessie on 17.07.2016.
- */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Lasst attermpt");
         System.out.println("Lasst attermpt");
     }
 }
