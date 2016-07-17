24c05178e3ad11041e3dfe6a28d127fd3ab15c33
flash9514 <max753942>
2016-07-17 20:33:22 +0300

    Added card class

---
+++ src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Card.java
@@ -0,0 +1,61 @@
+package com.innopolis.university.bootcamp2016.programmingA.texasholdem;
+
+/**
+ * Created by User on 17.07.2016.
+ */
+public class Card {
+
+    public enum CardRankEnum {
+        CARD_2(2),
+        CARD_3(3),
+        CARD_4(4),
+        CARD_5(5),
+        CARD_6(6),
+        CARD_7(7),
+        CARD_8(8),
+        CARD_9(9),
+        CARD_10(10),
+        JACK(11),
+        QUEEN(12),
+        KING(13),
+        ACE(14);
+
+        final int rank;
+        CardRankEnum(int p) {
+            rank = p;
+        }
+    }
+
+    public enum CardSuitEnum {
+        CLUBS(1),
+        DIAMONDS(2),
+        HEARTS(3),
+        SPADES(4);
+
+        final int suit;
+        CardSuitEnum(int p) {
+            suit = p;
+        }
+    }
+
+    private CardSuitEnum suit;
+    private CardRankEnum rank;
+
+    public Card(){
+
+    }
+
+    public Card(CardSuitEnum suit, CardRankEnum rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public CardSuitEnum getSuit() {
+        return suit;
+    }
+
+    public CardRankEnum getRank() {
+        return rank;
+    }
+
+}
