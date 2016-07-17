e7018b624c9c0e929dac79d1dafc29b0d5cb44c1
Jessie <farr.slmn@gmail.com>
2016-07-17 20:05:33 +0300

    1123

--- src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Main.java
+++ src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/Main.java
@@ -3,6 +3,5 @@ package com.innopolis.university.bootcamp2016.programmingA.texasholdem;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lasst attermpt");
-        System.out.println("Lasst attermpt");
     }
 }
