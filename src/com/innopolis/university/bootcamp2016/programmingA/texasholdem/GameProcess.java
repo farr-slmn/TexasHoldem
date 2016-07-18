91b705d58bc2a86d541bbc5734facc26fe3b8fdf
Jessie <farr.slmn@gmail.com>
2016-07-18 23:49:50 +0300

    Game added stages

--- src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/GameProcess.java
+++ src/com/innopolis/university/bootcamp2016/programmingA/texasholdem/GameProcess.java
@@ -1,8 +1,10 @@
 package com.innopolis.university.bootcamp2016.programmingA.texasholdem;
 
-public class Main {
+
+public class GameProcess {
     public static void main(String[] args) {
         Game game = new Game(50000, new Player("Tamara"), new Player("John"), new Player("Albert"), new Player("Zak"));
-        game.startGame();
+        game.initializeGame();
+        while(game.doStage() != Game.GameStage.END);
     }
 }
